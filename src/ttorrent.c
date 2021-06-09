43651a0aefc829e37d7c63614555d5d697c1f9c4
LoanPa <64327463+LoanPa@users.noreply.github.com>
2021-06-09 15:23:14 +0000

    484
    for (uint8_t i = 4; i < 12; i++) to for (uint8_t i = 5; i < 13; i++)
    
    >:-(

--- src/ttorrent.c
+++ src/ttorrent.c
@@ -481,7 +481,7 @@ int server(char* path, char* port)
 				log_printf(LOG_DEBUG, "recv()'d successfully");
 				
 				if(message[4] == MSG_REQUEST)	// Si el client demana un bloc, carreguem el número del bloc que demana
-					for (uint8_t i = 4; i < 12; i++)
+					for (uint8_t i = 5; i < 13; i++)
 					{
 						block_number <<= 8;
 						block_number |= message[i];
